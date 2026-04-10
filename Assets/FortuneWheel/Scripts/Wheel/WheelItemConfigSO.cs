55fbd46cf62a389ed98cbf8697c63518dc0aff3a
Murat Karaoglan <m.karaoglan1604@gmail.com>
2026-04-10 21:30:20 +0300

    Implemented OnValidate check to prevent "Skull" items in guaranteedItems and randomItemPool when IsSafeZone is active.

--- Assets/FortuneWheel/Scripts/Wheel/WheelItemConfigSO.cs
+++ Assets/FortuneWheel/Scripts/Wheel/WheelItemConfigSO.cs
@@ -1,32 +1,34 @@
+using System;
 using System.Collections.Generic;
+using FortuneWheel.Scripts.Item;
 using FortuneWheel.Scripts.Utils;
 using UnityEngine;
 
 namespace FortuneWheel.Scripts.Wheel
 {
-    [CreateAssetMenu(menuName = "Fortune Wheel/Wheel Item Config")]
+    [CreateAssetMenu(menuName = "Fortune Wheel/Wheel/Wheel Item Config")]
     public class WheelItemConfigSO : ScriptableObject
     {
         [field: SerializeField] public bool IsSafeZone { get; private set; }
         [field: SerializeField] public Sprite WheelSprite { get; private set; }
         [field: SerializeField] public Sprite WheelPointerSprite { get; private set; }
 
-        [Header("Guaranteed Items")]
-        [SerializeField] private List<WheelSliceItemData> guaranteedItems;
+        [Header("Guaranteed Items")] [SerializeField]
+        private List<WheelSliceItemData> guaranteedItems;
 
-        [Header("Random Pool")]
-        [SerializeField] private List<WheelSliceItemData> randomItemPool;
-        
-        public void PopulateSliceItems( List<WheelSliceItemData> cachedListItem,int sliceCount)
+        [Header("Random Pool")] [SerializeField]
+        private List<WheelSliceItemData> randomItemPool;
+
+        public void PopulateSliceItems(List<WheelSliceItemData> cachedListItem, int sliceCount)
         {
             cachedListItem.Clear();
-            
+
             var guaranteedCount = Mathf.Min(guaranteedItems.Count, sliceCount);
             for (var i = 0; i < guaranteedCount; i++)
             {
                 cachedListItem.Add(guaranteedItems[i]);
             }
-            
+
             var remaining = sliceCount - cachedListItem.Count;
             if (remaining > 0 && randomItemPool.Count > 0)
             {
@@ -37,8 +39,38 @@ namespace FortuneWheel.Scripts.Wheel
                     cachedListItem.Add(randomItemPool[i]);
                 }
             }
-            
+
             cachedListItem.Shuffle();
         }
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if (!IsSafeZone) return;
+ 
+            foreach (var wheelSliceItemData in guaranteedItems)
+            {
+                if (IsSkull(wheelSliceItemData))
+                {
+                    Debug.LogError("You can not add skull item the guaranteed list while zone is safe. Item deleted.");
+                    guaranteedItems.Remove(wheelSliceItemData);
+                    break;
+                }
+            }
+
+            foreach (var wheelSliceItemData in randomItemPool)
+            {
+                if (IsSkull(wheelSliceItemData))
+                {
+                    Debug.LogError("You can not add skull item the random pool list while zone is safe. Item deleted.");
+                    randomItemPool.Remove(wheelSliceItemData);
+                    break;
+                }
+            }
+#endif
+        }
+
+        private bool IsSkull(WheelSliceItemData wheelSliceItemData) =>
+            wheelSliceItemData.DropItem is MiscItemSO { IsPenalty: true };
     }
 }
