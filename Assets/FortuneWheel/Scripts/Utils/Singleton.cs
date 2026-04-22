c0047e759942b211a7a6f6805930a664f89c3e90
Murat Karaoglan <m.karaoglan1604@gmail.com>
2026-04-23 01:44:41 +0300

    fix: correct Singleton<T> duplicate instance check and remove unnecessary lock

--- Assets/FortuneWheel/Scripts/Utils/Singleton.cs
+++ Assets/FortuneWheel/Scripts/Utils/Singleton.cs
@@ -6,22 +6,16 @@ namespace FortuneWheel.Scripts.Utils
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
-        private static readonly object _lock = new object();
+
         public static T Instance
         {
             get
             {
                 if (_instance == null)
                 {
-                    lock (_lock)
-                    {
-                        _instance = FindObjectOfType<T>();
-                        if (_instance == null)
-                        {
-                            Debug.LogError($"No instance of {typeof(T)} found in the scene. Please make sure there is one in the scene or create one.");
-                        }
-                    }
+                    Debug.LogError($"No instance of {typeof(T)} found!");
                 }
+
                 return _instance;
             }
         }
@@ -32,7 +26,7 @@ namespace FortuneWheel.Scripts.Utils
             {
                 _instance = this as T;
             }
-            else if (_instance == this)
+            else if (_instance != this)
             {
                 Destroy(gameObject);
             }
